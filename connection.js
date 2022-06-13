32255eed078e95ea646ed7b2589dbef57251f904
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2022-06-13 23:52:49 +0100

    1st step

--- connection.js
+++ connection.js
@@ -4,14 +4,14 @@ const { MongoClient } = require('mongodb');
 
 async function main(callback) {
     const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
-    const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+    const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     try {
         // Connect to the MongoDB cluster
         await client.connect();
-
+       
         // Make the appropriate DB calls
         await callback(client);
+        console.log('connected');
 
     } catch (e) {
         // Catch any errors
@@ -20,4 +20,4 @@ async function main(callback) {
     }
 }
 
-module.exports = main;
+module.exports = main
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict';
-require('dotenv').config();
+require('dotenv').config('./db.env');
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const passport = require('passport')
+const session = require('express-session')
 
 const app = express();
 app.set('view engine', 'pug')
@@ -10,10 +13,46 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const ObjectID = require('mongodb').ObjectID
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'} );
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use(passport.initialize())
+app.use(passport.session()) 
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  app.route('/').get((req, res) => {
+    res.render( 'pug',
+     {
+      title: 'Connected to Database', 
+     message: 'Please login'
+    } );
+  })
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
 });
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
+});
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+})
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
